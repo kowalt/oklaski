adc62ab1bb20046db63b05191a14f7528aad9a91
Tomasz Kowal <kowalt7@gmail.com>
2021-02-15 00:47:27 +0100

    Criteria autocomplete

--- oklaski-web/src/app/app.component.ts
+++ oklaski-web/src/app/app.component.ts
@@ -11,21 +11,39 @@ import {Observable} from "rxjs";
 export class AppComponent implements OnInit {
   title = 'oklaski-web';
 
-  myControl = new FormControl();
-  options: string[] = ['siema', 'heniu'];
+  countryFormControl = new FormControl();
+  deviceFormControl = new FormControl();
+  countries: string[] = ['US', 'GB', 'JP'];
+  devices: string[] = ['iPhone 4',
+    'iPhone 4S',
+    'iPhone 5',
+    'Galaxy S3',
+    'Galaxy S4',
+    'Nexus 4',
+    'Droid Razor',
+    'Droid DNA',
+    'HTC One',
+    'iPhone 3'
+  ];
 
-  filteredOptions: Observable<string[]> | undefined;
+  filteredOptionsCountry: Observable<string[]> | undefined;
+  filteredOptionsDevices: Observable<string[]> | undefined;
 
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
+    this.filteredOptionsCountry = this.countryFormControl.valueChanges.pipe(
       startWith(''),
-      map(value => this._filter(value))
+      map(value => this._filter(value, this.countries))
+    );
+
+    this.filteredOptionsDevices = this.deviceFormControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value, this.devices))
     );
   }
 
-  private _filter(value: string): string[] {
+  private _filter(value: string, list: string[]): string[] {
     const filterValue = value.toLowerCase();
 
-    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+    return list.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
   }
 }
